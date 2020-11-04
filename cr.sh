6e7e4b87f2196ca13e109f1c80b9ae80eeaf4684
kali <ruslan.sot70@gmail.com>
2020-11-04 21:56:11 +0300

    Create Room script update

--- cr.sh
+++ cr.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # Creates folder with needed stuff for THM CTF
 
-NMAP=0
 for i in "$@"
 do
 case $i in
@@ -20,20 +19,37 @@ case $i in
 	shift
 	shift
 	;;
-	-n|--nmap)
-	NMAP=1
+    --ip=*)
+    TARGET_IP="${i#*=}"
 	shift
 	;;
+    --host=*)
+    DOMAIN_NAME="${i#*=}"
+    shift
+    ;;
 	-h|--help)
-	echo "-r|--room - \"Room Name\""
-	echo "-s|--sample - /path/to/sample/file"
-	echo "-u|--user - username"
-	echo "-h|--help - show this message"
-	echo "-n|--nmap - run nmap scan"
+	echo -e "Create Room Script: Do CTF Routine faster!\n\n
+    Options:\n
+    -r | --room - \"Room Name\"\n
+    -s | --sample - /path/to/sample/file\n
+    -u | --user - username\n
+    -h | --help - show this message\n
+    --ip - if provided, run NMAP scan on this IP. (Usage: --ip=<ip>)\n
+    --host - same as ip, but if you want to use Domain Name. (Usage: --host=<hostname.com>"
 	exit 0
 	;;
+    -*)
+    echo "Unknown option $1"
+    exit 1
+    ;;
 esac	
 done
+
+
+if [ ! -z "$DOMAIN_NAME"  -a  ! -z "$TARGET_IP" ]; then
+    echo "Do not use --host and --ip at the same time. Please choose one of this options"
+    exit 1
+fi
 
 if [ -z "$ROOM_NAME" ]; then
 	echo "Required param -r not set. See -h for help"
@@ -79,15 +95,32 @@ else
 fi
 
 
-if [ "$NMAP" -eq 1 ]; then
-	echo -e "\n\n====================="
+function validate_ip() {
+    octet="(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])"
+    ip4="^$octet\\.$octet\\.$octet\\.$octet$" 
+    [[ $1 =~ $ip4 ]] && echo 1 || echo 0
+}
+
+function nmap_scan() {
+    host=$1
+    
+	echo -e "\n====================="
 	echo "Performing NMAP Scan"
-	echo -e  "=====================\n\n"
-	echo "Command nmap -sC -A -oN $DIR_NAME/nmap/inital <IP>"
+	echo -e  "====================="
+    echo -e "HOST: $host \n"
+	nmap -sC -sV -A -oN $DIR_NAME/nmap/initial $host 
+}
 
-	read -r -p "IP: " IP
 
-	echo -e "\n\n"
-
-	nmap -sC -sV -A -oN $DIR_NAME/nmap/initial $IP
+if [ ! -z "$TARGET_IP" ]; then
+    if [ "$(validate_ip $TARGET_IP)" -eq "0" ]; then
+        while [ "$(validate_ip $TARGET_IP)" -eq "0" ]
+        do
+            read -r -p "Provided IP was invalid, try again: " TARGET_IP
+        done
+    fi
+    nmap_scan $TARGET_IP
+elif [ ! -z "$DOMAIN_NAME" ]; then
+    nmap_scan $DOMAIN_NAME
 fi
+
---
+++ sample-room/README.md
@@ -0,0 +1,3 @@
+[THM] Sample Room
+================
+Notes by undermouse, 04.11.2020
